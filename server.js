423ce3e706b98ee9522f124b8458374414072ca8
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-09-09 21:31:40 -0700

    fixed with linter

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 var express = require('express');
 var exphbs = require('express-handlebars');
 
-var db = require('./models');
-
-var app = express();
-var PORT = process.env.PORT || 3000;
+var db = require('./models'),
+  app = express(),
+  PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
@@ -13,23 +12,27 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Handlebars
-app.engine('handlebars', exphbs({
+app.engine(
+  'handlebars',
+  exphbs({
     defaultLayout: 'main',
     helpers: {
-        // Function to do basic mathematical operation in handlebar
-        math: function (lvalue, operator, rvalue) {
-            lvalue = parseFloat(lvalue);
-            rvalue = parseFloat(rvalue);
-            return {
-                "+": lvalue + rvalue,
-                "-": lvalue - rvalue,
-                "*": lvalue * rvalue,
-                "/": lvalue / rvalue,
-                "%": lvalue % rvalue
-            }[operator];
-        }
+      // Function to do basic mathematical operation in handlebar
+      math: function(lvalue, operator, rvalue) {
+        lvalue = parseFloat(lvalue);
+        rvalue = parseFloat(rvalue);
+
+        return {
+          '+': lvalue + rvalue,
+          '-': lvalue - rvalue,
+          '*': lvalue * rvalue,
+          '/': lvalue / rvalue,
+          '%': lvalue % rvalue
+        }[operator];
+      }
     }
-}));
+  })
+);
 app.set('view engine', 'handlebars');
 
 // Routes
@@ -38,21 +41,23 @@ require('./routes/htmlRoutes')(app);
 
 var syncOptions = { force: false };
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
+/*
+ * If running a test, set syncOptions.force to true
+ * clearing the `testdb`
+ */
 if (process.env.NODE_ENV === 'test') {
-    syncOptions.force = true;
+  syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-    app.listen(PORT, function () {
-        console.log(
-            '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
-            PORT,
-            PORT
-        );
-    });
+db.sequelize.sync(syncOptions).then(function() {
+  app.listen(PORT, function() {
+    console.log(
+      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+      PORT,
+      PORT
+    );
+  });
 });
 
 module.exports = app;
--- test/canary.test.js
+++ test/canary.test.js
@@ -1,8 +1,10 @@
-var expect = require('chai').expect;
+var expect = require('chai');
 
 describe('canary test', function() {
-  // A "canary" test is one we set up to always pass
-  // This can help us ensure our testing suite is set up correctly before writing real tests
+  /*
+   * A "canary" test is one we set up to always pass
+   * This can help us ensure our testing suite is set up correctly before writing real tests
+   */
   it('should pass this canary test', function() {
     expect(true).to.be.true;
   });
