15e9ce4d9f8e95a61c5f009c687beeb8e907384d
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-09-09 20:11:27 -0700

    handlebars code

--- server.js
+++ server.js
@@ -13,12 +13,23 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Handlebars
-app.engine(
-  'handlebars',
-  exphbs({
-    defaultLayout: 'main'
-  })
-);
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    helpers: {
+        // Function to do basic mathematical operation in handlebar
+        math: function (lvalue, operator, rvalue) {
+            lvalue = parseFloat(lvalue);
+            rvalue = parseFloat(rvalue);
+            return {
+                "+": lvalue + rvalue,
+                "-": lvalue - rvalue,
+                "*": lvalue * rvalue,
+                "/": lvalue / rvalue,
+                "%": lvalue % rvalue
+            }[operator];
+        }
+    }
+}));
 app.set('view engine', 'handlebars');
 
 // Routes
@@ -30,18 +41,18 @@ var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === 'test') {
-  syncOptions.force = true;
+    syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
-      PORT,
-      PORT
-    );
-  });
+db.sequelize.sync(syncOptions).then(function () {
+    app.listen(PORT, function () {
+        console.log(
+            '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
+            PORT,
+            PORT
+        );
+    });
 });
 
 module.exports = app;
